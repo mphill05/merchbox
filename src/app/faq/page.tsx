47adad94c380941e977bf46edb8b8514976d3f8a
Mark <mphill05@gmail.com>
2023-10-05 15:01:23 -0500

    layout for faq page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './page.module.scss';
+import Accordian from '@/components/Accordian/Accordian';
+import { faqData } from '@/constants';
 
 const FaqPage = () => {
-  return <div>FaqPage</div>;
+  return (
+    <div className={styles.FaqMain}>
+      <div>
+        <h3>OUR MOST FREQUENTLY ASKED QUESTIONS</h3>
+      </div>
+      {faqData.map((data, index) => (
+        <div key={index}>
+          <Accordian title={data.title} content={data.content} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default FaqPage;
