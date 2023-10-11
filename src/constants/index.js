789ed2531d8e4d3684e7f0c7b3b265e7dc989d74
Mark <mphill05@gmail.com>
2023-10-11 14:14:35 -0500

    teampage update

--- src/constants/index.js
+++ src/constants/index.js
@@ -227,3 +227,48 @@ export const faqData = [
     content: `Yep! We're happy to do so and have done it for many of our clients. You'll need to cover the cost of shipping, but we always find the most cost effective option for you!`
   }
 ]
+
+export const teamData = [
+  {
+    image: '/lurtz.jpg',
+    name: 'LURTZ',
+    title: 'FINISHING SERVICES & DTG OPERATOR',
+    desc: 'LURTZ MAKES SURE THE QUALITY OF THE FINISHED PRODUCTS ARE UP TO THE MERCHBOX STANDARD BEFORE THEY LEAVE THE HQ!'
+  },
+  {
+    image: '/jelani.jpg',
+    name: 'JELANI',
+    title: 'LOGISTICS MANAGER',
+    desc: 'JELANI HANDLES SHIPPING, FULFILLMENT, and CUSTOMER SERVICE FOR our CLIENT WEBSITES, AND MAKES SURE their CUSTOMERS receive their products in a timely manner!'
+  },
+  {
+    image: '/justin.jpg',
+    name: 'JUSTIN',
+    title: 'CLIENT ONBOARDING',
+    desc: 'JUSTIN IS AT THE FRONT LINES OF COMMUNICATION AND WILL reply to your CONTACT FORM request! he then COLLECTs ALL of THE NECESSARY ASSETS TO MAKE YOUR PROJECT COME TO LIFE!'
+  },
+  {
+    image: '/jonnie.jpg',
+    name: 'JONNIE',
+    title: 'OPERATIONS SPECIALIST',
+    desc: `catch JONNIE OPERATing ALL OF OUR MACHINERy, MAKing YOUR DESIGNS become a reality! SHE'S GOT MANY YEARS OF EXPERIENCE, AND CAN PRINT ON ALL SORTS OF GARMENTS!`
+  },
+  {
+    image: '/mauricio.jpg',
+    name: 'mauricio',
+    title: 'SCREEN PRINTING SPECIALIST & FINISHING SERVICES',
+    desc: `MAURICIO IS A JACK OF ALL TRADES - WHEN HE ISN'T HELPING JONNIE ON THE SCREEN PRINTING MACHINE, YOU CAN FIND HIM ASSISTING LURTZ ON THE FINISHING SERVICES!`
+  },
+  {
+    image: '/nina.jpg',
+    name: 'nina',
+    title: 'HUMAN RESOURCES',
+    desc: 'NINA IS OUR HR MANAGER AND MAKES SURE OUR CREW IS ALWAYS HAPPY THROUGHOUT THE DAY!'
+  },
+  {
+    image: '/steven.jpg',
+    name: 'steven',
+    title: 'PROJECT MANAGER',
+    desc: `STEVEN HAS BEEN WITH MERCHBOX FROM THE VERY BEGINNING AND OVERSEES THE ENTIRE HQ MAKING SURE EVERYTHING IS RUNNING ON SCHEDULE! HE'S SEEN AND DONE IT ALL, AND MERCHBOX WOULDN'T BE WHAT IT IS TODAY WITHOUT HIM!`
+  },
+]
