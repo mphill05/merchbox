677b6c9cc7917e5d672621827f54213765824222
Mark <mphill05@gmail.com>
2023-10-11 19:55:40 -0500

    styling for contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,66 +1,147 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import styles from './page.module.scss';
+import 'font-awesome/css/font-awesome.min.css';
 
 const ContactPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState('CHOOSE AN OPTION');
+  const [selectedOption, setSelectedOption] = useState(false);
+
+  const options = ['<100', '100-500', '500-1000', '1000-5000', '5000+'];
+
+  const handleClickOutside = (event: any) => {
+    if (event.target.closest('.selectWrapper') === null) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleOptionClick = (option: any) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <div className={styles.contactPage}>
       <div className={styles.formHeader}>LET&apos;S WORK</div>
       <div className={styles.contactForm}>
         <form className={styles.form}>
-          <div>
-            <label htmlFor="Name" title="Name">
-              NAME *
-            </label>
-            <input id="Name" type="text" placeholder="OTTO" required />
-            <label htmlFor="Email" title="Email">
-              EMAIL *
-            </label>
-            <input
-              id="Email"
-              type="email"
-              placeholder="HI@MERCHBOX.INFO"
-              required
-            />
+          <div className={styles.nameEmailContainer}>
+            <div className={styles.nameContainer}>
+              <label htmlFor="Name" title="Name">
+                NAME *
+              </label>
+              <input id="Name" type="text" placeholder="OTTO" required />
+            </div>
+            <div className={styles.emailContainer}>
+              <label htmlFor="Email" title="Email">
+                EMAIL *
+              </label>
+              <input
+                id="Email"
+                type="email"
+                placeholder="HI@MERCHBOX.INFO"
+                required
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="Company/Artist Name" title="Company/Arist Name">
-              Company/Artist Name *
-            </label>
-            <input
-              id="Company/Artist Name"
-              type="text"
-              placeholder="MERCHBOX"
-            ></input>
-            <label htmlFor="Quantities" title="Quantities">
-              DESIRE QUANTITIES *
-            </label>
-            <select id="Quantities" name="quantities">
-              <option value={''}>CHOOSE AN OPTION</option>
-              <option value={1}>100</option>
-              <option value={2}>100-500</option>
-              <option value={3}>500-1000</option>
-              <option value={4}>1000-5000</option>
-              <option value={5}>5000+</option>
-            </select>
+          <div className={styles.companyAndQuantitiesContainer}>
+            <div className={styles.artistContainer}>
+              <label htmlFor="Company/Artist Name" title="Company/Arist Name">
+                Company/Artist Name *
+              </label>
+              <input
+                id="Company/Artist Name"
+                type="text"
+                placeholder="MERCHBOX"
+              ></input>
+            </div>
+            <div className={styles.quantitiesContainer}>
+              <label htmlFor="Quantities" title="Quantities">
+                DESIRE QUANTITIES *
+              </label>
+              <div
+                className={`${styles.selectWrapper} ${
+                  selectedOption ? `${styles.optionSelected}` : ''
+                }`}
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <div className={styles.selectedValue}>
+                  {selectedValue}
+                  {isOpen ? (
+                    <i className={`fa fa-angle-up ${styles.caret}`}></i>
+                  ) : (
+                    <i className={`fa fa-angle-down ${styles.caret}`}></i>
+                  )}
+                </div>
+                {isOpen && (
+                  <div className={styles.options}>
+                    {options.map((option, index) => (
+                      <div
+                        key={index}
+                        onClick={() => {
+                          setSelectedValue(option);
+                          setSelectedOption(true);
+                          setIsOpen(false);
+                        }}
+                        className={styles.option}
+                      >
+                        {option}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-          <label htmlFor="projectDesc">PROEJCT DESCRIPTION *</label>
-          <textarea
-            id="projectDesc"
-            name="projectDesc"
-            rows={2}
-            cols={30}
-            placeholder='EX: "200 BLACK T-SHIRT WITH LOGO ON THE FRONT LEFT AND A LARGE BACK PRINT WITH PRINTED NECK TAGS. I NEED THEM FOR MY TOUR IN NOVEMBER'
-          ></textarea>
-          <div className="bottomForm">
+          <div className={styles.projectContainer}>
+            <label htmlFor="projectDesc">PROEJCT DESCRIPTION *</label>
+            <textarea
+              id="projectDesc"
+              name="projectDesc"
+              rows={2}
+              cols={30}
+              placeholder='EX: "200 BLACK T-SHIRT WITH LOGO ON THE FRONT LEFT AND A LARGE BACK PRINT WITH PRINTED NECK TAGS. I NEED THEM FOR MY TOUR IN NOVEMBER'
+            ></textarea>
+          </div>
+          <div className="mockUpContainer">
             <label className="mock-up" htmlFor="mock-up">
               MOCK UP *
             </label>
-            <input id="mock-up" type="file" name="file" />
-            <p>MAX FILE SIZE 15MB</p>
+            <label htmlFor="mock-up" className={styles.customMockUpBtn}>
+              UPLOAD HERE +
+            </label>
+            <input
+              id="mock-up"
+              type="file"
+              name="file"
+              className={styles.mockUpBtn}
+              style={{ display: 'none' }}
+            />
+            <p className={styles.mockUpUnderText}>MAX FILE SIZE 15MB</p>
             {/* Add captcha here */}
-            <button type="submit" value={'Submit'}>
-              Send
-            </button>
+            <div className={styles.submitBtnContainer}>
+              <button
+                className={styles.submitBtn}
+                type="submit"
+                value={'Submit'}
+              >
+                Send!
+              </button>
+            </div>
           </div>
         </form>
       </div>
