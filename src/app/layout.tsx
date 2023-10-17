f6a7fd7805899d70e3d45d5d87bd3bc6ed573da6
Mark <mphill05@gmail.com>
2023-10-17 00:37:37 -0500

    print page styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import Navbar from '../components/Navbar/Navbar';
+import Footer from '@/components/Footer/Footer';
 import type { Metadata } from 'next';
 import '../globals.scss';
-import Footer from '@/components/Footer/Footer';
+import '../customFonts.scss';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
